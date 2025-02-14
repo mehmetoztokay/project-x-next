ef7f9ad5afa3caeee6278bed946bdef08c5943f0
mehmetoztokay <mehmetoztokay@gmail.com>
2025-02-14 16:38:57 +0300

    Updated modal component

--- src/app/[locale]/(iframes)/iframe/register-form/FormArea.tsx
+++ src/app/[locale]/(iframes)/iframe/register-form/FormArea.tsx
@@ -5,22 +5,175 @@ import { Modal } from "@/components/Atoms/Modal";
 
 export const FormArea = () => {
   const [isOpenModal, setIsOpenModal] = useState<boolean>(false);
+  const [isOpenModal2, setIsOpenModal2] = useState<boolean>(false);
 
   return (
     <>
       {/* {t("title")} */}
       <RegisterForm />
       <Modal isOpenModal={isOpenModal} setIsOpenModal={setIsOpenModal}>
-        <div className="mx-auto flex h-[80%] w-[80%] items-center rounded-md bg-white p-4 lg:h-1/2 lg:w-1/2">
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ex et
-            dolorem saepe necessitatibus harum minima non magnam qui eius cum,
-            laudantium aliquam! Adipisci libero omnis dolor delectus numquam
-            reiciendis?
-          </p>
+        <div className="flex w-[500px] items-center rounded bg-white p-4 pt-14">
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ex
+              et dolorem saepe necessitatibus harum minima non magnam qui eius
+              cum, laudantium aliquam! Adipisci libero omnis dolor delectus
+              numquam reiciendis?
+            </p>
+            <button
+              onClick={() => setIsOpenModal2(true)}
+              className="rounded bg-blue-400 px-4 py-4"
+            >
+              Open modal
+            </button>
+          </div>
         </div>
       </Modal>
-      <button onClick={() => setIsOpenModal(true)}>Open modal</button>
+      <Modal
+        isOpenModal={isOpenModal2}
+        setIsOpenModal={setIsOpenModal2}
+        hasCancelAbortAlert
+      >
+        <div className="flex w-[500px] items-center rounded bg-white p-4 pt-14">
+          <div className="">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Soluta
+              qui eaque ducimus voluptate magnam voluptates animi ea incidunt.
+              Omnis asperiores possimus perspiciatis rem molestiae quaerat qui
+              aperiam placeat tempora reiciendis fugit, sequi perferendis, illo
+              tempore eos quam culpa nemo ex eaque provident quis assumenda
+              incidunt consequuntur ipsam! Beatae similique totam libero ex,
+              earum laboriosam vitae sapiente cum deleniti! Incidunt quam
+              doloremque sapiente amet delectus, cupiditate porro modi animi
+              nisi ipsa quod, repudiandae nostrum vitae pariatur quos!
+              Repudiandae at corrupti assumenda ut, blanditiis praesentium nisi
+              fuga itaque nostrum maxime voluptatibus aut dolores inventore
+              dolorum tempora! Deleniti fuga vel minima quasi necessitatibus?
+              Rerum ad nesciunt in ducimus incidunt illo dolores harum nulla est
+              itaque atque placeat quod et mollitia, saepe hic quidem. Natus
+              esse consequuntur nam ipsum veritatis pariatur, iste velit
+              corporis obcaecati rem hic quod eos est aliquid dolore placeat
+              nobis. Ex amet nesciunt a, repellat magni vero architecto dolorem,
+              quam itaque deserunt tenetur similique quaerat natus perspiciatis
+              repudiandae, assumenda adipisci optio aspernatur iure ut alias
+              nemo. Esse, adipisci! A id ipsum, rem esse consectetur veniam
+              ipsa. Eligendi voluptate officia earum, expedita iusto id
+              dignissimos explicabo nesciunt consequatur, iste debitis dolorem
+              repellendus sunt facere, cumque aspernatur laborum alias adipisci
+              totam corporis? Necessitatibus magnam quae accusamus voluptatum
+              culpa sunt quas explicabo ratione nisi quo illum inventore,
+              numquam, aliquid excepturi repudiandae consequuntur perspiciatis
+              eius autem. Fuga nisi aliquid aperiam nobis molestiae hic
+              cupiditate error, quibusdam fugit doloremque? Tempore est sed
+              beatae at necessitatibus voluptas ullam itaque, provident
+              consequatur commodi exercitationem eius vero quibusdam laudantium
+              iure nobis numquam a ad repellat rerum quam et vel dicta? Velit
+              ipsam incidunt optio aut. Quasi dignissimos ducimus enim natus in
+              officiis porro nihil quae! Repellendus deleniti perspiciatis totam
+              quaerat aspernatur veniam ratione ducimus odio aliquam, vitae
+              accusantium voluptatibus facere enim cupiditate doloribus. Cumque,
+              nam deleniti? Vel magni dicta odit impedit voluptatibus nemo eum,
+              minus fugiat voluptas corrupti. Distinctio in quaerat nulla
+              corporis? Culpa deserunt ratione maiores possimus, fugit, impedit
+              minima ea voluptatum vitae, accusantium neque corporis nulla
+              voluptatibus! Exercitationem eveniet consequuntur beatae odit nam
+              earum obcaecati unde voluptatum, voluptas deserunt est
+              consectetur. Doloribus, provident dicta. Asperiores, iusto minima
+              quidem vitae dolorum cum ratione reprehenderit eveniet dicta,
+              mollitia perferendis. Earum, similique voluptas nemo deserunt
+              perferendis dolorum maxime soluta molestias, labore atque debitis
+              quaerat quam nulla eveniet voluptatum! Natus eligendi, sequi nobis
+              consectetur ex at iste commodi ut esse facilis id necessitatibus
+              dolorem perferendis vitae magnam optio. Sint laudantium pariatur
+              quos! Molestias, error adipisci repellat et officia omnis cumque
+              iure dolorem laudantium saepe, enim deserunt quod perferendis
+              eligendi voluptatem minus vitae vel veniam sunt accusamus
+              repellendus quos, iste ex! Harum ad earum ab ut pariatur non
+              delectus eum alias autem fuga veritatis tempore nam, quod
+              obcaecati laborum architecto numquam sit omnis? Libero omnis iure
+              eius at, sapiente repellat explicabo ex quis vero doloremque
+              deserunt, earum hic expedita quasi velit! Voluptatem, nesciunt
+              eligendi enim dolore quidem numquam dolor possimus saepe qui sint?
+              Culpa delectus facilis dolor, aperiam quas explicabo cum! Vitae
+              quaerat repudiandae repellendus, sapiente deleniti assumenda sequi
+              inventore nobis doloribus et culpa voluptatum dignissimos magnam
+              blanditiis. Dolore iste cupiditate quidem doloremque accusantium
+              aspernatur! Minima a inventore non ipsum id, aperiam tempora
+              adipisci magni reiciendis sunt doloremque, aliquam numquam.
+              Perspiciatis dicta modi adipisci. Neque id quibusdam est
+              molestias, veritatis possimus facilis, aspernatur aperiam optio
+              blanditiis eaque tempore debitis, accusantium eum autem sapiente
+              dolorum eveniet corporis. Reiciendis soluta eligendi ratione
+              tempore natus adipisci aspernatur! Nostrum repellat aperiam eos,
+              reiciendis suscipit nam doloribus, cupiditate in dignissimos
+              consectetur quibusdam est. Autem quam totam ducimus obcaecati,
+              animi odio quibusdam repellat quisquam vero, esse atque maxime
+              quidem corrupti tempore nihil. Autem iste harum error nulla
+              laudantium nobis, provident velit iusto voluptatibus soluta
+              accusantium atque asperiores incidunt fuga quos! Animi autem
+              officiis, repellat itaque nisi unde voluptate consectetur
+              reiciendis consequuntur rem deleniti obcaecati quis et illo, nam,
+              quod totam iste. Quaerat nostrum iste pariatur a optio commodi
+              perspiciatis corporis, quas architecto porro, beatae molestias
+              inventore accusamus id minus possimus autem consequatur rem?
+              Architecto dolorem, esse cum molestias delectus et doloremque
+              possimus iste quibusdam molestiae adipisci rem ipsum nisi.
+              Mollitia odio nam repudiandae praesentium quidem harum, inventore
+              natus magnam voluptas cum, ut consectetur ab temporibus culpa.
+              Incidunt error, vel molestias est expedita consectetur nisi
+              possimus deserunt mollitia quia perferendis unde ab soluta magnam
+              non delectus sapiente veritatis qui illo quis maxime! Impedit illo
+              sequi ea excepturi fugiat quaerat unde dolores illum incidunt.
+              Cumque commodi dolore suscipit et necessitatibus mollitia fuga id,
+              illum sint rerum autem quidem facere aperiam, quisquam molestiae
+              iure vel quam veniam? Et iure reprehenderit totam, voluptas quia
+              delectus recusandae beatae nisi maxime officia voluptatibus
+              obcaecati voluptate a qui odit amet numquam eveniet cupiditate
+              aspernatur nobis aliquid vero reiciendis? Reiciendis eaque fugiat
+              repellendus quod odit aut repellat tenetur asperiores dolore
+              necessitatibus consectetur ut eligendi omnis illum rem eos neque
+              quibusdam fuga error, earum dolor quia, corporis id! Tempora
+              consequatur dolorem assumenda, ab ipsa deserunt tempore, nemo
+              exercitationem blanditiis hic aliquid possimus quae non officiis
+              inventore veritatis itaque aperiam incidunt placeat porro
+              molestiae at magni. Nostrum consectetur, quas qui similique porro,
+              modi ipsam natus distinctio, placeat omnis quidem? Quia unde
+              necessitatibus itaque adipisci quasi magnam nulla sit! Aspernatur
+              tempore, quasi sint cumque et quia aperiam enim voluptatem
+              deleniti corrupti nisi vitae consectetur accusamus repellat modi
+              laborum. Distinctio fugiat consequuntur eius velit sequi harum non
+              quas odit illo quaerat? Laudantium accusamus, laboriosam ipsam
+              possimus nulla culpa vero. Cupiditate unde, eaque aliquam quia
+              ipsam impedit id dolores delectus ducimus, sit minus. Molestias
+              vero, labore quia saepe ipsam voluptatem laudantium modi illum?
+              Beatae commodi, libero nobis minus tenetur soluta a necessitatibus
+              doloremque accusantium numquam blanditiis eum quod labore nulla
+              fugit error animi nemo molestiae? Suscipit est obcaecati modi,
+              impedit corporis possimus eius alias dolorem itaque! Hic natus
+              amet, tempora ex numquam, vero temporibus quam accusamus ipsam
+              veritatis officia dolorem, sunt dolorum obcaecati ipsum rerum
+              iusto! Ea dolorem animi nobis, libero corrupti iste error beatae
+              reprehenderit aliquam officiis numquam quibusdam? Sequi esse
+              suscipit eveniet blanditiis quisquam sed voluptatum cumque. Ut
+              tenetur unde ea quod error repellat dignissimos magni! Nihil, rem!
+            </p>
+            <button
+              onClick={() => setIsOpenModal2(true)}
+              className="rounded bg-blue-400 px-4 py-4"
+            >
+              Open modal
+            </button>
+          </div>
+        </div>
+      </Modal>
+      <div className="my-4 flex items-center justify-center">
+        <button
+          className="mx-auto rounded bg-blue-400 px-4 py-4"
+          onClick={() => setIsOpenModal(true)}
+        >
+          Open modal 1
+        </button>
+      </div>
     </>
   );
 };
--- src/components/Atoms/Modal/index.tsx
+++ src/components/Atoms/Modal/index.tsx
@@ -1,29 +1,185 @@
-import { Dispatch, SetStateAction, ReactNode } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  ReactNode,
+  useEffect,
+  useState,
+} from "react";
+import { createPortal } from "react-dom";
 
 type ModalProps = {
   isOpenModal: boolean;
   setIsOpenModal: Dispatch<SetStateAction<boolean>>;
   children: ReactNode;
+  onOpen?: () => void;
+  onClose?: () => void;
+  hasCancelAbortAlert?: boolean;
+  alertMessage?: string;
+  abortAlertButtonText?: string;
+  continueAlertButtonText?: string;
+  paddingY?: string;
 };
+
+const modalStack: { close: () => void; id: string }[] = [];
 
 export const Modal: React.FC<ModalProps> = ({
   isOpenModal,
   setIsOpenModal,
   children,
+  onOpen,
+  onClose,
+  hasCancelAbortAlert = false,
+  alertMessage = "Are you sure you want to cancel?",
+  abortAlertButtonText = "Abort",
+  continueAlertButtonText = "Continue",
+  paddingY,
 }) => {
+  const [showAlert, setShowAlert] = useState(false);
+  const modalId = Math.random().toString(36).substr(2, 9); // Unique id for each modal
+
+  useEffect(() => {
+    if (isOpenModal) {
+      // Push new modal with unique id into the stack
+      modalStack.push({ close: () => setIsOpenModal(false), id: modalId });
+      toggleOverflowHiddenClassToBody();
+      onOpen?.();
+    }
+
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        if (showAlert) {
+          setShowAlert(false);
+        } else if (hasCancelAbortAlert) {
+          setShowAlert(true);
+        } else {
+          // Only close the topmost modal from stack
+          const topModal = modalStack[modalStack.length - 1];
+          if (topModal && topModal.id === modalId) {
+            topModal.close();
+            onClose?.();
+          }
+        }
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [
+    isOpenModal,
+    setIsOpenModal,
+    onOpen,
+    onClose,
+    showAlert,
+    hasCancelAbortAlert,
+    modalId,
+  ]);
+
+  const toggleOverflowHiddenClassToBody = () => {
+    document.querySelector("body")?.classList.toggle("overflow-hidden");
+  };
+
+  const handleClose = () => {
+    if (hasCancelAbortAlert) {
+      setShowAlert(true); // Eğer hasCancelAbortAlert varsa, alert açılır
+    } else {
+      // Close current modal and remove it from stack
+      const index = modalStack.findIndex((modal) => modal.id === modalId);
+      if (index !== -1) {
+        modalStack.splice(index, 1);
+      }
+      setIsOpenModal(false);
+      onClose?.();
+      toggleOverflowHiddenClassToBody();
+    }
+  };
+
+  const handleAbort = () => {
+    setShowAlert(false);
+    setIsOpenModal(false);
+    onClose?.();
+    toggleOverflowHiddenClassToBody();
+  };
+
+  const handleContinue = () => {
+    setShowAlert(false);
+  };
+
   if (!isOpenModal) return null;
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="animate-fade-in relative flex h-full w-full transform items-center justify-center rounded-lg transition-transform">
-        <button
-          className="absolute right-5 top-5 text-gray-200"
-          onClick={() => setIsOpenModal(false)}
-        >
-          Close Modal
-        </button>
-        {children}
+  const modalContent = (
+    <div className="fixed inset-0 z-[1050] overflow-hidden overflow-y-auto overflow-x-hidden bg-black bg-opacity-70 backdrop-blur-md">
+      <div
+        className="relative h-full w-auto translate-x-0 translate-y-0 transform pt-28 transition-all duration-300 ease-out"
+        style={{ paddingTop: paddingY ? paddingY.toString() : "" }}
+      >
+        <div className="pointer-events-auto relative flex h-full w-full flex-col bg-clip-padding">
+          <div className="relative m-auto">
+            <div>
+              <button
+                className="absolute right-5 top-5 flex h-8 w-8 items-center justify-center rounded-full bg-black bg-opacity-70 text-gray-800 backdrop-blur-sm hover:bg-opacity-90"
+                onClick={handleClose}
+              >
+                <div className="w-1/2">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    height="100%"
+                    viewBox="0 -960 960 960"
+                    width="100%"
+                    className="fill-gray-400"
+                  >
+                    <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                  </svg>
+                </div>
+              </button>
+              {showAlert && (
+                <div className="absolute right-5 top-3 mt-2 w-[70%] max-w-[300px] rounded-md bg-black bg-opacity-80 p-5 pt-8 text-sm text-gray-200 backdrop-blur-sm">
+                  <button
+                    className="bg-bl absolute right-3 top-3 flex h-6 w-6 items-center justify-center rounded-full bg-black bg-opacity-30 backdrop-blur-md hover:bg-opacity-60"
+                    onClick={() => setShowAlert(false)}
+                  >
+                    <div className="w-1/2">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        height="100%"
+                        viewBox="0 -960 960 960"
+                        width="100%"
+                        className="fill-gray-400"
+                      >
+                        <path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" />
+                      </svg>
+                    </div>
+                  </button>
+                  <p>{alertMessage}</p>
+                  <div className="mt-2 flex gap-4">
+                    <button
+                      className="underline underline-offset-2"
+                      onClick={handleContinue}
+                    >
+                      {continueAlertButtonText}
+                    </button>
+                    <button
+                      className="text-red-500 opacity-70"
+                      onClick={handleAbort}
+                    >
+                      {abortAlertButtonText}
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div
+              className="mb-28"
+              style={{ marginBottom: paddingY ? paddingY.toString() : "" }}
+            >
+              {children}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
+
+  return createPortal(modalContent, document.body);
 };
